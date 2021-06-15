50e60a60e17b6c4ea32a9f094af8824ff8440573
aleksandar-ristic <aristic665@gmail.com>
2021-06-16 01:38:27 +0200

    added nav menu and sections js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,82 @@
+/* *** Nav Menu *** */
+;(() => {
+	const hamburgerBtn = document.querySelector('.hamburger-btn')
+	const navMenu = document.querySelector('.nav-menu')
+	const closeNavBtn = navMenu.querySelector('.close-nav-menu')
+
+	hamburgerBtn.addEventListener('click', showNavMenu)
+	closeNavBtn.addEventListener('click', hideNavMenu)
+
+	function showNavMenu() {
+		navMenu.classList.add('open')
+		toggleScroll()
+	}
+
+	function hideNavMenu() {
+		navMenu.classList.remove('open')
+		fadeOutEffect()
+		toggleScroll()
+	}
+
+	function fadeOutEffect() {
+		document.querySelector('.fade-out-effect').classList.add('active')
+
+		setTimeout(() => {
+			document.querySelector('.fade-out-effect').classList.remove('active')
+		}, 300)
+	}
+
+	// attach event handler to document
+	document.addEventListener('click', e => {
+		if (e.target.classList.contains('link-item')) {
+			// make sure e.target.hash has a value before overriding default behavior
+			if (e.target.hash !== '') {
+				// anchor links default override
+				e.preventDefault()
+				const hash = e.target.hash
+				console.log(hash)
+				//deactivate existing active 'section'
+				document.querySelector('.section.active').classList.add('hide')
+				document.querySelector('.section.active').classList.remove('active')
+
+				//activate new 'section'
+				document.querySelector(hash).classList.add('active')
+				document.querySelector(hash).classList.remove('hide')
+
+				// deactivate existing active navigation menu 'link-item'
+				navMenu
+					.querySelector('.active')
+					.classList.add('outer-shadow', 'hover-in-shadow')
+				navMenu
+					.querySelector('.active')
+					.classList.remove('active', 'inner-shadow')
+
+				// if clicked 'link-item is contained within the nav menu'
+				if (navMenu.classList.contains('open')) {
+					// activate new navigation menu 'link-item'
+					e.target.classList.add('active', 'inner-shadow')
+					e.target.classList.remove('outer-shadow', 'hover-in-shadow')
+
+					//hide nav menu
+					hideNavMenu()
+				} else {
+					let navItems = navMenu.querySelectorAll('.link-item')
+					navItems.forEach(item => {
+						if (hash === item.hash) {
+							// activate new navigation menu 'link-items'
+							item.classList.add('active', 'inner-shadow')
+							item.classList.remove('outer-shadow', 'hover-in-shadow')
+						}
+					})
+					fadeOutEffect()
+				}
+				// add hash to url
+				window.location.hash = hash
+			}
+		}
+	})
+})()
+
 /* *** About Section Tabs *** */
 ;(() => {
 	const aboutSection = document.querySelector('.about-section'),
@@ -24,6 +103,10 @@
 		}
 	})
 })()
+
+function toggleScroll() {
+	document.body.classList.toggle('scroll-stop')
+}
 
 /* *** Portfolio Filter and  Popup *** */
 ;(() => {
@@ -102,6 +185,7 @@
 		popup.classList.toggle('open')
 		toggleScroll()
 	}
+
 	// gets img index from buttons and shows a img from
 	// screenshots array
 	function popupSlideshow() {
@@ -233,4 +317,14 @@
 		sliderContainer.style.marginLeft = -(slideWidth * slideIndex) + 'px'
 	}
 	slider()
+})()
+
+/* *** Hide all sections except active *** */
+;(() => {
+	const sections = document.querySelectorAll('.section')
+	sections.forEach(section => {
+		if (!section.classList.contains('active')) {
+			section.classList.add('hide')
+		}
+	})
 })()
