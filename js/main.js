85812becfe7cb7003576ca33f773dd420b4fba53
aleksandar-ristic <aristic665@gmail.com>
2021-06-18 16:17:42 +0200

    Added preloader element

--- js/main.js
+++ js/main.js
@@ -328,3 +328,10 @@ function toggleScroll() {
 		}
 	})
 })()
+
+window.addEventListener('load', () => {
+	document.querySelector('.preloader').classList.add('fade-out')
+	setTimeout(() => {
+		document.querySelector('preloader').style.display = 'none'
+	}, 600)
+})
